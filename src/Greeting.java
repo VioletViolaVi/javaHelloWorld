d052e343ebab4c8d42cb1e7369aedf8dae6fe820
VioletViola <vivian.v18723@gmail.com>
2021-08-17 20:59:55 +0100

    Add greeting class.

---
+++ src/Greeting.java
@@ -0,0 +1,5 @@
+public class Greeting {
+    public String sayHi(String name) {
+        return "Hello " + name + "!";
+    }
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Greeting greet = new Greeting();
+        System.out.println(greet.sayHi("Regina"));
     }
 }
